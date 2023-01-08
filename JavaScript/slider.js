767b8e3bb099f81b777a3b86172a7f7bd500700c
Syartaa <syartapajaziti@gmail.com>
2023-01-07 18:10:30 -0800

    formimi i sliderit

---
+++ JavaScript/slider.js
@@ -0,0 +1,40 @@
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+
+}
+
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
