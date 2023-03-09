9d584f28a55d203afe0364b990246e491c0cc42c
jeta <jetagagica@gmail.com>
2023-03-09 20:09:24 +0100

    lidhjen e loginit me databaze

--- login.php
+++ login.php
@@ -1,3 +1,35 @@
+
+<?php
+ 
+
+ if(isset($_POST['submit'])){
+    require_once 'config.php';
+    if(empty($_POST['email']) || empty($_POST['password'])){
+        $error[] = 'Please fill all fields!';}
+    else{$email=$_POST['email']; 
+    $password=$_POST['password'];
+    $select ="SELECT * FROM register WHERE email = '$email' and password='$password' ";
+    $result=mysqli_query($conn,$select);
+        if($result){
+            $num=mysqli_num_rows($result);//tells the number of rows present inside my database
+            if($num>0){
+                session_start();
+                $_SESSION['email']=$email;
+                header('location:home.php');
+                
+ 
+         else{
+            $error[] = 'incorrect email or password!';
+         }
+    }else{
+        $error[]="user doesn't  exist!";
+    }
+}
+ }
+}
+?> 
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,19 +43,43 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Moon+Dance&display=swap" rel="stylesheet">
     <title>Document</title>
+    <style>
+        .register{
+            text-align: center;
+        }
+        .registerbtn{
+            color: #d89538;
+            list-style: none;
+}
+.error-msg{
+    margin:10px 0px;
+    display:block;
+    background:crimson;
+    color: #fff;
+    border-radius:5px;
+    font-size:20px;
+    padding:5px;
+}
+    </style>
 </head>
 
 <body>
 
     <div class="wrapper">
-        <header class="header">
+       
         <?php include './include/navbar.php' ?>
-        </header>
-
+     
         <main>
             <div class="container">
-                <form id="form" action="/">
+                <form action="login.php" id="form" method='POST'>
                     <h1>Log in</h1>
+                    <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
                     <div class="input-control">
                         <label for="email">Email</label>
                         <input id="email" name="email" type="text">
@@ -34,18 +90,18 @@
                         <input id="password" name="password" type="password">
                         <div class="error"></div>
                     </div>
-                    <button type="submit">Log in</button>
+                    <button type="submit" name="submit">Login</button>
                 </form>
-                <a href="./register.php" class="signup">Registere</a>
+               <p class="register" > Don't have an account? <a href="./register.php" class="registerbtn">Registere</a> </p>
         </main>
 
 
-        <footer class="footer">
+        
         <?php include './include/footer.php' ?>
-        </footer>
+       
     </div>
 
-    <script src="./JavaScript/login.js"></script>
+    <!-- <script src="./JavaScript/login.js"></script> -->
 </body>
 
 </html>
