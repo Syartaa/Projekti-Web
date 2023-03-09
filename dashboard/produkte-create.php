071301eac7909eb81de60188361c4c84b0a4ed73
jonakrs <jonakrasniqi1411@gmail.com>
2023-03-09 20:27:50 +0100

    krijimi i produkteve me ane te databazes

---
+++ dashboard/produkte-create.php
@@ -0,0 +1,80 @@
+<?php
+require_once 'productsController.php';
+
+$produkte = new productsController;
+if(isset($_POST['submit'])){
+    $produkte->insert($_POST);
+}
+?>
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <style>
+            *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body{
+    background-color: antiquewhite;
+   margin: 0;
+}
+    form{
+        width: 400px;
+    margin: 8vh auto 5vh auto;
+    padding: 20px;
+    background-color: rgba(242, 205, 161, 0.6);
+    border-radius: 4px;
+    font-size: 12px;
+    box-shadow: 10px -5px 5px rgb(243, 207, 162);
+    }
+    form input{
+     margin:5px;
+     padding:5px;
+    }
+   form label{
+        font-size:15px;
+
+    } 
+    .save{
+        background-color: rgba(231, 133, 22, 0.6);
+        border-radius:5px;
+        border:none;
+        width:100%;
+        padding:5px;
+    } 
+    .save:hover{
+        background-color: rgba(231, 133, 22, 0.9);
+    }
+   
+
+</style>
+    </head>
+    <body>
+    
+    <?php include '../include/navbar.php' ?>
+  <main>
+<form action="" method="POST">
+        <label>Image:</label>
+        <input type="file" name="image" >
+        <br>
+        <label>Title:</label>
+        <input type="text" name="title" >
+        <br>
+       <label> name:</label>
+        <input type="text" name="name">
+        <br> 
+        <label>Description:</label>
+        <textarea name="description" cols="30" rows="5"></textarea>
+        <br>
+    <input type="submit" name="submit" class="save" value="SAVE" >
+</form>
+</main>
+     <?php include '../include/footer.php' ?>  
+    
+    </html>
