732080a3fc592c2a663aff7f5ec3e3194d6cb95a
jonakrs <jonakrasniqi1411@gmail.com>
2023-03-09 20:14:30 +0100

    krijimi i klases databaze

---
+++ dashboard/database.php
@@ -0,0 +1,18 @@
+
+<?php
+
+class Database{
+    public $pdo;
+
+    public function __construct(){
+        try{
+            // session_start();
+            $link = new PDO('mysql:host=localhost;dbname=products','root','');
+            $this->pdo=$link;
+        }catch(PDOException $e){
+            die($e->getMessage());
+        }
+    }
+}
+
+?>
